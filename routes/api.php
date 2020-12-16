43d21940980e56c878d2e988ac24a8af204675f6
aron010 <aron.raes@hotmail.com>
2020-12-16 22:47:23 +0100

    adding api and start of api authorisation

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+use Orion\Facades\Orion;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Route::get('user', function (Request $request) {
+    //     return $request->user();
+    // });
+
+    Route::group(['as' => 'api.'], function () {
+        Orion::resource('posts', PostsController::class);
+    });
 });
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
