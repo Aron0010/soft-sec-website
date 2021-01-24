8a2cebb321dc4c0f5afc8b8a40bfa0ee2ffa1bdc
aron010 <aron.raes@hotmail.com>
2021-01-24 14:32:41 +0100

    re-add alpine

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
   <!-- Scripts -->
-
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
 </head>
 
 <body>
